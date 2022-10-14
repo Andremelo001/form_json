393ff51bb3906f199a73ba802d53a518a7dc958e
Andremelo001 <93006884+Andremelo001@users.noreply.github.com>
2022-10-14 15:23:03 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,32 +1,13 @@
 <?php
 
+//convertendo os dados em json
 if(isset($_POST['submit'])){
 
 $new_message = array(
 
     "desc" => $_POST['desc'],
     "client" => $_POST['client'],
-    "cpf" => $_POST['cpf'],
-    "inscricao" => $_POST['inscricao'],
-    "uf" => $_POST['uf'],
-    "cnpj" => $_POST['cnpj'],
-    "city" => $_POST['city'],
-    "bairro" => $_POST['bairro'],
-    "endereco" => $_POST['endereco'],
-    "cep" => $_POST['cep'],
-    "number" => $_POST['number'],
-    "cell" => $_POST['cell'],
-    "fant" => $_POST['fant'],
-    "obs" => $_POST['obs'],
-    "email" => $_POST['email'],
-    "limite" => $_POST['limite'],
-    "contribuinte" => $_POST['contribuinte'],
-    "data" => $_POST['data'],
-    "status" => $_POST['status'],
-    "vencimento" => $_POST['vencimento'],
-    "solicitacao" => $_POST['solicitacao'],
-    "prorrogacao" => $_POST['prorrogacao'],
-    "chave" => $_POST['chave'],
+    "cpf" => $_POST['cpf']
 
 );
 
@@ -47,7 +28,6 @@ if (filesize("Result_json/messages.json") == 0) {
 
 if (!file_put_contents("Result_json/messages.json", json_encode($data_to_save, JSON_PRETTY_PRINT), LOCK_EX)){
     
- 
     $success = "Dados inseridos com sucesso!";
     
 }else{
@@ -55,20 +35,21 @@ if (!file_put_contents("Result_json/messages.json", json_encode($data_to_save, J
     $error = "Erro encontrado, tente novamente!!";
     
 }
-    
-    $json= json_encode($dados);
 
-print_r($json);
+//Inserindo o json no banco
+
+$json= json_encode($data_to_save);
+
 $curl = curl_init();
 
 curl_setopt_array($curl,[
-    CURLOPT_URL=>"http://localhost/webserviceandre/Result_json/messages.json",
+    CURLOPT_URL=>"http://localhost/WebServiceInfo/Result_json/messages.json",
     CURLOPT_CUSTOMREQUEST => "POST",
     CURLOPT_POSTFIELDS => $json,
     CURLOPT_RETURNTRANSFER=>true,
     CURLOPT_POST=>true,
     CURLOPT_HTTPHEADER => [
-        'Content-Type: application/json',
+        'Content-Type: application/json', 
         'Content-Length: '.strlen($json)
     ],
     CURLOPT_HEADER=>true,
@@ -80,6 +61,5 @@ try {
 } catch (Exception $e) {
     echo $e;
 }
-    
-}
 
+}
