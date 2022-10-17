5d830b0049bed0bdee560f0dfe96429a83cd322a
Andremelo001 <93006884+Andremelo001@users.noreply.github.com>
2022-10-17 16:27:52 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,6 +1,11 @@
 <?php
+//conectar com o banco
+$mysqli = new mysqli('localhost', 'root', '', 'json_test');
+if ($mysqli->connect_errno  != 0) {
+    echo $mysqli->connect_error;
+}
 
-//convertendo os dados em json
+//convertendo os dados do form para json
 if(isset($_POST['submit'])){
 
 $new_message = array(
@@ -19,7 +24,7 @@ if (filesize("Result_json/messages.json") == 0) {
 
 }else{
 
-    $old_records = json_decode(file_get_contents("Result_json/messages.json"));
+    $old_records = json_decode(file_get_contents("Result_json/messages.json"), JSON_OBJECT_AS_ARRAY);
 
     array_push($old_records, $new_message);
 
@@ -36,9 +41,38 @@ if (!file_put_contents("Result_json/messages.json", json_encode($data_to_save, J
     
 }
 
+//inserindo o json no banco
+$stmt = $mysqli->prepare
+(
+    "INSERT INTO teste (descricao, cliente, cpf) 
+     VALUES (?,?,?)"
+);
+$stmt->bind_param("ssdi", $descricao, $cliente, $cpf);
+
+
+//percorrendo os dados do json
+$inserted_rows = 0;
+foreach ((array)$data_to_save as $old) {
+    $descricao = $old["descricao"];
+    $cliente = $old["cliente"];
+    $cpf = $old["cpf"];
+
+    $stmt->execute();
+    $inserted_rows ++;
+}
+
+if (count($data_to_save) == $inserted_rows) {
+    echo "success";
+}else{
+    echo "error";
+}
+
+}
+
+
 //Inserindo o json no banco
 
-$json= json_encode($data_to_save);
+/*$json= json_encode($data_to_save);
 
 $curl = curl_init();
 
@@ -57,9 +91,7 @@ curl_setopt_array($curl,[
     ]);
 try {
    $response = curl_exec($curl);
-    echo $response;
+   $consulta = "INSERT INTO teste (descricao,cliente,cpf) VALUES ('$response');";
 } catch (Exception $e) {
     echo $e;
-}
-
-}
+}*/
