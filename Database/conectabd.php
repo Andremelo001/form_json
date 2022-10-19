a17f9a35936ec9f46708685832193f826c767d98
Andremelo001 <93006884+Andremelo001@users.noreply.github.com>
2022-10-19 14:31:25 -0300

    Delete Database directory

--- Database/conectabd.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'database.php';
-
-try {
-    $connPdo = new \PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-} catch (PDOException $e) {
-    die("Não foi possível se conectar ao banco de dados $dbname :" . $e->getMessage());
-}
--- Database/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $host = 'localhost';
-    $dbname = 'json_test';
-    $username = 'root';
-    $password = '';
