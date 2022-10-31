f812ca5ec2b0d6e8002fcf565ca33062e344eb0e
Andremelo001 <93006884+Andremelo001@users.noreply.github.com>
2022-10-31 13:32:18 -0300

    Update inserir.php

--- inserir.php
+++ inserir.php
@@ -1,5 +1,6 @@
 <?php
-//conectar com o banco
+//conecta com o banco
+//usar PDO, mysqli é antigo e pode entrar em desuso
 $mysqli = new mysqli('localhost', 'root', '', 'json_test');
 if ($mysqli->connect_errno  != 0) {
     echo $mysqli->connect_error;
@@ -12,7 +13,32 @@ $new_message = array(
 
     "desc" => $_POST['desc'],
     "client" => $_POST['client'],
-    "cpf" => $_POST['cpf']
+    "cpf" => $_POST['cpf'],
+    "estadual" => $_POST['estadual'],
+    "cnpj" => $_POST['cnpj'],
+    "uf" => $_POST['uf'],
+    "city" => $_POST['city'],
+    "bairro" => $_POST['bairro'],
+    "endereco" => $_POST['endereco'],
+    "cep" => $_POST['cep'],
+    "number" => $_POST['number'],
+    "tell" => $_POST['tell'],
+    "cell" => $_POST['cell'],
+    "fantasy" => $_POST['fantasy'],
+    "obs" => $_POST['obs'],
+    "email" => $_POST['email'],
+    "credito" => $_POST['credito'],
+    "contribuinte" => $_POST['contribuinte'],
+    "date_cadastro" => $_POST['date_cadastro'],
+    "status" => $_POST['status'],
+    "inativo" => $_POST['inativo'],
+    "chave" => $_POST['chave'],
+    "date_vencimento" => $_POST['date_vencimento'],
+    "date_solicitacao" => $_POST['date_solicitacao'],
+    "date_prorrogacao" => $_POST['date_prorrogacao'],
+    "chave_prorrogacao" => $_POST['chave_prorrogacao'],
+    "id_user" => $_POST['id_user'],
+    //27
 
 );
 
@@ -44,32 +70,55 @@ if (!file_put_contents("Result_json/messages.json", json_encode($data_to_save, J
 //inserindo o json no banco
 $stmt = $mysqli->prepare
 (
-    "INSERT INTO teste (descricao, cliente, cpf) 
-     VALUES (?,?,?)"
+    "INSERT INTO teste (descricao, cliente, cpf, incricao_estadual, cnpj, uf, cidade, bairro, endereco, cep, numero, telefone, celular, nome_fantasia, obs, email, limite_credito, tipo_contribuinte, data_cadastro, status_cliente, inativo, chave, data_vencimento, data_solicitacao, data_prorrogacao, chave_prorrogacao, usuario_id) 
+     VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
 );
-$stmt->bind_param("ssd", $descricao, $cliente, $cpf);
-
+$stmt->bind_param("sssssssssssssssssssssssssss", $descricao, $cliente, $cpf, $estadual, $cnpj, $uf, $city, $bairro, $endereco, $cep, $number, $tell, $cell, $fantasy, $obs, $email, $credito, $contribuinte, $date_cadastro, $status, $inativo, $chave, $date_vencimento, $date_solicitacao, $date_prorrogacao, $chave_prorrogacao, $id_user);
 
 //percorrendo os dados do json
 $inserted_rows = 0;
-foreach ((array)$data_to_save as $data) {
+foreach ($data_to_save as $data) {
     $descricao = $data["desc"];
     $cliente = $data["client"];
     $cpf = $data["cpf"];
+    $estadual = $data["estadual"];
+    $cnpj = $data["cnpj"];
+    $uf = $data["uf"];
+    $city = $data["city"];
+    $bairro = $data["bairro"];
+    $endereco = $data["endereco"];
+    $cep = $data["cep"];
+    $number = $data["number"];
+    $tell = $data["tell"];
+    $cell = $data["cell"];
+    $fantasy = $data["fantasy"];
+    $obs = $data["obs"];
+    $email = $data["email"];
+    $credito = $data["credito"];
+    $contribuinte = $data["contribuinte"];
+    $date_cadastro = $data["date_cadastro"];
+    $status = $data["status"];
+    $inativo = $data["inativo"];
+    $chave = $data["chave"];
+    $date_vencimento = $data["date_vencimento"];
+    $date_solicitacao = $data["date_solicitacao"];
+    $date_prorrogacao = $data["date_prorrogacao"];
+    $chave_prorrogacao = $data["chave_prorrogacao"];
+    $id_user = $data["id_user"];
 
     $stmt->execute();
     $inserted_rows ++;
 }
 
 if (count($data_to_save) == $inserted_rows) {
-    echo "success";
+    echo "Dados inseridos com sucesso";
 }else{
     echo "error";
 }
 
 }
 
-//funciona, porém ele sobreescreve os dados anteriores :(
+//funciona, porém ele sobreescreve os dados anteriores, obs: é igual o de cima, só que  o código é menor kkkkkkkk :(
 //percorrendo os dados do json;
 /*foreach ((array)$data_to_save as $data) {
  
@@ -78,6 +127,9 @@ if (count($data_to_save) == $inserted_rows) {
     mysqli_query($mysqli, $query);
 }*/
 
+
+
+//deve funcionar, porém eu não tenho a menor ideia de como fazer funciona
 //Inserindo o json no banco
 
 /*$json= json_encode($data_to_save);
@@ -103,3 +155,4 @@ try {
 } catch (Exception $e) {
     echo $e;
 }*/
+
